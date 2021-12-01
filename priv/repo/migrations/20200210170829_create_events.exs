2355bb12420aad2a630a408c22d1888784beba0f
Miguel C <miquecg@users.noreply.github.com>
2021-12-01 14:34:31 +0100

    Define database tables

--- priv/repo/migrations/20200210170829_create_events.exs
+++ priv/repo/migrations/20200210170829_create_events.exs
@@ -2,11 +2,16 @@ defmodule NaiveDice.Repo.Migrations.CreateEvents do
   use Ecto.Migration
 
   def change do
-    create table(:events) do
-      add :title, :string
+    create table("events") do
+      add :title, :varchar, null: false
       add :allocation, :integer, null: false
 
       timestamps()
     end
+
+    create constraint("events",
+      :allocation_non_negative,
+      check: "allocation >= 0"
+    )
   end
 end
