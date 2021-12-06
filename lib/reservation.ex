6eb15814400016747154e9d1c64c38a1ac02ca21
Miguel C <miquecg@users.noreply.github.com>
2021-12-06 20:42:11 +0100

    Complete GuestController

--- lib/reservation.ex
+++ lib/reservation.ex
@@ -8,11 +8,16 @@ defmodule Reservation do
   alias Reservation.Payment
   alias Reservation.Payment.CheckoutSession
   alias Reservation.Repo
-  alias Reservation.Schemas.{Event, Order}
+  alias Reservation.Schemas.{Event, Order, Ticket}
 
   def all_events_info(keys) do
     events = Repo.all(Event)
     Enum.map(events, &build_info(&1, keys))
+  end
+
+  def all_tickets_info(keys) do
+    tickets = Repo.all(Ticket)
+    Enum.map(tickets, &build_info(&1, keys))
   end
 
   def event_info(event_id, keys) do
@@ -67,14 +72,27 @@ defmodule Reservation do
     end
   end
 
-  defp build_info(event, :remaining_tickets = k, acc) do
-    # Event must exist on EventBooker if it exists on database.
-    {:ok, remaining} = EventBooker.remaining_tickets(event.id)
-    Map.put(acc, k, remaining)
+  @doc """
+  Exposed for testing.
+  """
+  def reset do
+    {_, nil} = Repo.delete_all(Order)
+    {_, nil} = Repo.delete_all(Ticket)
+    Supervisor.stop(Reservation.EventBookerSupervisor, :kill)
   end
 
-  defp build_info(event, key, acc) when is_map_key(event, key) do
-    Map.put(acc, key, Map.get(event, key))
+  defp build_info(schema, keys) do
+    Enum.reduce(keys, %{}, &build_info(schema, &1, &2))
+  end
+
+  defp build_info(%Event{id: event_id}, :remaining_tickets = key, acc) do
+    # Event must exist on EventBooker if it exists on database.
+    {:ok, remaining} = EventBooker.remaining_tickets(event_id)
+    Map.put(acc, key, remaining)
+  end
+
+  defp build_info(schema, key, acc) when is_map_key(schema, key) do
+    Map.put(acc, key, Map.get(schema, key))
   end
 
   defp build_info(_, _, acc), do: acc
