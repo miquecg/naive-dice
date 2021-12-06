5f662d35de2f84d6f281872b773c4fea993db3c8
Miguel C <miquecg@users.noreply.github.com>
2021-12-06 20:44:38 +0100

    Fix missing key in all_events_info call

--- lib/naive_dice_web/controllers/event_controller.ex
+++ lib/naive_dice_web/controllers/event_controller.ex
@@ -2,8 +2,7 @@ defmodule NaiveDiceWeb.EventController do
   use NaiveDiceWeb, :controller
 
   def index(conn, _params) do
-    events = Reservation.all_events_info([:title])
-
+    events = Reservation.all_events_info([:id, :title])
     render(conn, "index.html", %{events: events})
   end
 end
