def93776dc75989647075d13ba44bfed0a399a4b
Miguel C <miquecg@users.noreply.github.com>
2021-12-06 23:26:34 +0100

    Move environment config to runtime.exs

--- config/prod.exs
+++ config/prod.exs
@@ -1,12 +1,9 @@
 use Mix.Config
 
 config :naive_dice, NaiveDiceWeb.Endpoint,
-  http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "mysterious-meadow-6277.herokuapp.com", port: 443],
+  server: true,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
-
-import_config "prod.secret.exs"
