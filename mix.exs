fcad26a0f2a1562a4ad44e2ae868d7e25c2186e6
Miguel C <miquecg@users.noreply.github.com>
2021-12-14 22:50:44 +0100

    Project builds with Elixir 1.13

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule NaiveDice.MixProject do
     [
       app: :naive_dice,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
